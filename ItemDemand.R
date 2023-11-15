1440191ff9c7b8ee25bb5bf4950faee131940bdc
Jacob Smith <22jacobsmith@gmail.com>
2023-11-15 13:34:42 -0700

    Create ML models for store 7 item 32

--- ItemDemand.R
+++ ItemDemand.R
@@ -1,37 +1,160 @@
 # ItemDemand.R
 
+
+## LIBRARIES
+
+
 library(tidyverse)
 library(tidymodels)
 library(patchwork)
 library(timetk)
 library(vroom)
 
+
+## READ IN DATA
+
+
 train <- vroom("train.csv")
 test <- vroom("test.csv")
 
+### EDA - plot ACF
 
-plot1 <- train %>% filter(store == 5, item == 1) %>%
+plot1 <- train %>% filter(store == 7, item == 32) %>%
   pull(sales) %>%
   forecast::ggAcf(., lag.max = 365)
 
-plot2 <- train %>% filter(store == 2, item == 10) %>%
+plot2 <- train %>% filter(store == 1, item == 1) %>%
   pull(sales) %>%
-  forecast::ggAcf(., lag.max = 365)
+  forecast::ggAcf(., lag.max = 30)
 
-plot3 <- train %>% filter(store == 3, item == 7) %>%
+plot3 <- train %>% filter(store == 7, item == 7) %>%
   pull(sales) %>%
-  forecast::ggAcf(., lag.max = 365)
+  forecast::ggAcf(., lag.max = 30)
 
-plot4 <- train %>% filter(store == 10, item == 5) %>%
+plot4 <- train %>% filter(store == 5, item == 6) %>%
   pull(sales) %>%
-  forecast::ggAcf(., lag.max = 365)
+  forecast::ggAcf(., lag.max = 30)
 
 plot5 <- (plot1 + plot2) / (plot3 + plot4)
 
+
+# output to plot window
 plot5
+
+
+### pick a store and an item for exploration, building a model
+
+# store 7, item 32
+nStores <- max(train$store)
+nItems <- max(train$item)
+
+store_item <-
+  train %>% filter(store == 7, item == 32)
+
+
+ts_recipe <- recipe(sales~., data = store_item) %>%
+  step_date(date, features = "dow") %>%
+  #step_date(date, features = "decimal") %>%
+  step_date(date, features = "doy") %>%
+  step_range(date_doy, min = 0, max = pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy))
+
+knn_model <- nearest_neighbor(neighbors = tune()) %>%
+  set_engine("kknn") %>%
+  set_mode("regression")
+
+
+
+knn_wf <- 
+  workflow() %>%
+  add_model(knn_model) %>%
+  add_recipe(ts_recipe)
+
+
+tuning_grid <- grid_regular(neighbors(),
+                            levels = 5)
+
+
+
+## split into folds
+folds <- vfold_cv(store_item, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  knn_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(smape))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("smape")
+
+collect_metrics(CV_results) %>%
+  filter(neighbors == 10) %>%
+  pull(mean)
+
+# RF
+
+
+ts_recipe <- recipe(sales~., data = store_item) %>%
+  step_date(date, features = "dow") %>%
+  #step_date(date, features = "decimal") %>%
+  step_date(date, features = "doy") %>%
+  step_range(date_doy, min = 0, max = pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy))
+
+rf_model <- rand_forest(min_n = tune(), mtry = 3) %>%
+  set_engine("ranger") %>%
+  set_mode("regression")
+
+
+
+rf_wf <- 
+  workflow() %>%
+  add_model(rf_model) %>%
+  add_recipe(ts_recipe)
+
+
+tuning_grid <- grid_regular(min_n(),
+                            levels = 5)
+
+
+
+## split into folds
+folds <- vfold_cv(store_item, v = 5, repeats = 1)
+
+# run cv
+
+CV_results <-
+  rf_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(smape))
+
+# find best tuning parm values
+
+best_tune <-
+  CV_results %>%
+  select_best("smape")
+
+collect_metrics(CV_results) %>%
+  filter(min_n == 30) %>%
+  pull(mean)
+
+
+### FOR LOOP
+
 
 nStores <- max(train$store)
 nItems <- max(train$item)
+
+
+# main double-loop to set up store-item pairs
+
 for(s in 1:nStores){
   for(i in 1:nItems){
     storeItemTrain <- train %>%
